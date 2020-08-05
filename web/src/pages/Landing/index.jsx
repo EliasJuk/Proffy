5110fcd8344e22319f57dbc11d7ed72acb6853d7
Elias Juk <eliasp.juk@hotmail.com>
2020-08-05 16:22:31 -0300

    🔖 Layout

--- web/src/pages/Landing/index.jsx
+++ web/src/pages/Landing/index.jsx
@@ -32,7 +32,7 @@ function Landing() {
 
           <a href="#" className="give-classes">
             <img src={giveClassesIcon} alt="Esudar" />
-              Estudar
+              Dar aulas
           </a>           
         </div>
 
