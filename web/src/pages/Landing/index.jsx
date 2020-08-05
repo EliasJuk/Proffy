3a8c93d863a99e2f58bed9c57ca1ed6e50d92f96
Elias Juk <eliasp.juk@hotmail.com>
2020-08-05 16:16:19 -0300

    🔖 Layout

--- web/src/pages/Landing/index.jsx
+++ web/src/pages/Landing/index.jsx
@@ -17,9 +17,12 @@ function Landing() {
         <div className="logo-container">
           <img src={logoImg} alt="Proffy"/>
           <h2>Sua plataforma de estudos online.</h2>
-        
-          <img src={landingImg} alt="Plataforma de estudos" className="hero-image" />
         </div>
+
+        <img 
+            src={landingImg} 
+            alt="Plataforma de estudos" 
+            className="hero-image" />
 
         <div className="buttons-container">
           <a href="#" className="study">
