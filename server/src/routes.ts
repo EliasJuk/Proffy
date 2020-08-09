511624f56c5e2baa84415d45b989fb0da6483d0c
Elias Juk <eliasp.juk@hotmail.com>
2020-08-09 16:15:47 -0300

    Tratamento dos dados a serem inseridos no banco

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -25,37 +25,48 @@ routes.post('/classes', async (req, res) => {
   //TRX => DB
   const trx = await db.transaction(); 
 
-  const insertedUsersIds = await trx('users').insert({
-    name,
-    avatar,
-    whatsapp,
-    bio,
-  });
-  const user_id = insertedUsersIds[0];
+  try {
+    const insertedUsersIds = await trx('users').insert({
+      name,
+      avatar,
+      whatsapp,
+      bio,
+    });
+    const user_id = insertedUsersIds[0];
+  
+  
+    const insertedClassesIds = await trx('classes').insert({
+      subject,
+      cost,
+      user_id,
+    })
+    const class_id = insertedClassesIds[0];
+  
+  
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+      return {
+        class_id,
+        week_day: scheduleItem.week_day,
+        from: convertHourToMinutes(scheduleItem.from),
+        to: convertHourToMinutes(scheduleItem.to),
+      };
+    })
+  
+    await trx('class_schedule').insert(classSchedule);
+  
+    await trx.commit();
+  
+    return res.status(201).send();
 
+  } catch(err) {
+    await trx.rollback(); //Desfaz qualquer alterações caso haja algum erro
 
-  const insertedClassesIds = await trx('classes').insert({
-    subject,
-    cost,
-    user_id,
-  })
-  const class_id = insertedClassesIds[0];
+    //console.log(err);
+    return res.status(400).json({
+      error: 'Unexpected error while creating new class'
+    })
+  }
 
-
-  const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-    return {
-      class_id,
-      week_day: scheduleItem.week_day,
-      from: convertHourToMinutes(scheduleItem.from),
-      to: convertHourToMinutes(scheduleItem.to),
-    };
-  })
-
-  await trx('class_schedule').insert(classSchedule);
-
-  await trx.commit();
-
-  return res.send();
 });
 
 export default routes;
