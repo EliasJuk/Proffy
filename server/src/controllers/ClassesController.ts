bb80aeb3c0d8d94b0007eaecdb9e4fd78e980664
Elias Juk <eliasp.juk@hotmail.com>
2020-08-09 17:57:36 -0300

    Controllers

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, Response, response } from 'express';
 
 import db from '../database/connection';
 
@@ -11,6 +11,38 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
+  async index(req: Request, res: Response) {
+    const filters = req.query;
+
+    //Tipagens
+    const subject = filters.subject as string;
+    const week_day = filters.week_day as string;
+    const time = filters.time as string;
+
+    if (!filters.week_day || !filters.subject || !filters.time) {
+      return response.status(400).json({
+        error: "Missing filters to search classes"
+      })
+    }
+    
+    const timeInMinutes = convertHourToMinutes(time)
+
+    const classes = await db('classes')
+      .whereExists(function() {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+      })
+      .where('classes.subject', '=', subject)
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .select(['classes.*', 'users.*']);
+            
+    return res.json(classes);
+  }
+
   async create (req: Request, res: Response) {
     const {
       name,
