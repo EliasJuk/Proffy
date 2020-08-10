6cc8b80a882edfaeef3ce8a809f501fd6fd26c32
Elias Juk <eliasp.juk@hotmail.com>
2020-08-10 17:00:55 -0300

    Componentização

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,6 +8,7 @@ import './styles.css';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 //FC = FunctionComponent
@@ -24,6 +25,8 @@ const PageHeader: React.FC <PageHeaderProps> = (props) => {
 
         <div className="header-content">
           <strong>{props.title}</strong>
+            { /* props.description ? <p>{props.description}</p> : null */}
+            { props.description && <p>{props.description}</p> }
 
           {props.children}
         </div>
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
+import './styles.css';
+
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warning from '../../assets/images/icons/warning.svg';
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrivel que Você quer Dar aulas. " />
+      <PageHeader 
+        title="Que incrivel que Você quer Dar aulas. " 
+        description="O primeiro passo é preencher esse formulário de inscrição"      
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+            <Input name="name" label="Nome completo" id="name"/>
+            <Input name="avatar" label="Avatar" id="avatar"/>   
+            <Input name="whatsapp" label="WhatsApp" id="whatsapp"/>  
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+            <Input name="subject" label="Matéria"/>
+            <Input name="cost" label="Custo da sua hora por aula"/>
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warning} alt="Aviso Inportante" />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar cadastro
+          </button>
+        </footer>
+      </main>
     </div>
   )
 }
