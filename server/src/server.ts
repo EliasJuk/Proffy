97ba4273277678aee3bdde4b8bd64fe24e4e58a3
Elias Juk <eliasp.juk@hotmail.com>
2020-08-12 16:47:50 -0300

    Alterar PORT

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,8 +4,9 @@ import routes from './routes';
 
 const app = express();
 
-app.use(express.json());
-app.use(routes);
 app.use(cors());
 
-app.listen(3333);
+app.use(express.json());
+app.use(routes);
+
+app.listen(8080);
